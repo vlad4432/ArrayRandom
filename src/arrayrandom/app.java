9e617a58eecfd21cea925130d54d83a082849728
pupil <pupil@ivkhk.ee>
2019-02-28 11:37:47 +0300

    Работа с массивам в Java

---
+++ src/arrayrandom/app.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayrandom;
+
+import java.util.Random;
+
+/**
+ *
+ * @author pupil
+ */
+public class app {
+    public void {
+
+    Random r = new Random();
+    int[] myArray = new int[20];
+    int n;
+    for(int i = 0; i < myArray.; i++){
+        do{
+             n = r.nextInt(100);
+            if{n%2==0}{
+                break;
+            }
+        }while(true);
+        myArray[i] = n;
+}
+    for(int i=0; i<myArray.length;i++){
+    
+}
+    
+
+
+
+} 
+}
